34d140f235bd1f8a175773b20f3197459b2eae77
tugberkgoc <tugberkgoc@gmail.com>
2018-12-01 00:59:30 +0300

    Initial Commit

---
+++ client/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <div class="jumbotron">
+      <h1 class="display-3">Welcome to Community!</h1>
+      <p class="lead">A fully open source forum. Made with by Coding Garden.</p>
+      <hr class="my-4">
+      <p>This forum is all about community. Be kind! Be helpful!</p>
+      <p class="lead">
+        <a class="btn btn-primary btn-lg" href="#" role="button">Sign Up</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+}
+
+</script>
