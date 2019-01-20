da91dff84dbfea8a9cd3af0cf1ad16069f0792e7
tugberkgoc <tugberkgoc@gmail.com>
2019-01-20 17:45:28 +0300

    Conf State Management with VUEX

--- client/src/components/AgGrid.vue
+++ client/src/components/AgGrid.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+
     <div style="height: 100%">
       <div style="height: 100%; box-sizing: border-box;">
         <ag-grid-vue style="width: 100%; height: 100%;" class="ag-theme-balham" id="myGrid"
@@ -13,14 +14,13 @@
                      :floatingFilter="true"></ag-grid-vue>
       </div>
     </div>
+
   </div>
 </template>
 
 <script>
-  import { AgGridVue } from "ag-grid-vue"
-  import axios from 'axios'
-
-  //import "ag-grid-enterprise";
+  import { AgGridVue } from "ag-grid-vue";
+  import axios from 'axios';
 
   export default {
     name: 'AgGrid',
@@ -112,11 +112,11 @@
           filter: "agDateColumnFilter",
           filterParams: {
             comparator: (filterLocalDateAtMidnight, cellValue) => {
-              var dateParts = cellValue.split("/");
-              var day = Number(dateParts[2]);
-              var month = Number(dateParts[1]) - 1;
-              var year = Number(dateParts[0]);
-              var cellDate = new Date(day, month, year);
+              const dateParts = cellValue.split("/");
+              const day = Number(dateParts[2]);
+              const month = Number(dateParts[1]) - 1;
+              const year = Number(dateParts[0]);
+              const cellDate = new Date(day, month, year);
               if (cellDate < filterLocalDateAtMidnight) {
                 return -1;
               } else if (cellDate > filterLocalDateAtMidnight) {
@@ -132,37 +132,13 @@
 
     mounted() {
       this.gridApi = this.gridOptions.api;
-      this.gridColumnApi = this.gridOptions.columnApi;
+      // this.gridColumnApi = this.gridOptions.columnApi;
     },
 
     methods: {
-      onGridReady(params) {
-        const httpRequest = new XMLHttpRequest();
-        const updateData = data => {
-          this.rowData = data;
-          value = value + 1;
-        };
-
-        const data = [
-          {"athlete":"Michael Phelps","age":23,"country":"United States","year":2008,"date":"24/08/2008","sport":"Swimming","gold":8,"silver":0,"bronze":0,"total":8},
-          {"athlete":"Michael Phelps","age":19,"country":"United States","year":2004,"date":"29/08/2004","sport":"Swimming","gold":6,"silver":0,"bronze":2,"total":8},
-          {"athlete":"Michael Phelps","age":27,"country":"United States","year":2012,"date":"12/08/2012","sport":"Swimming","gold":4,"silver":2,"bronze":0,"total":6},
-          {"athlete":"Natalie Coughlin","age":25,"country":"United States","year":2008,"date":"24/08/2008","sport":"Swimming","gold":1,"silver":2,"bronze":3,"total":6},
-          {"athlete":"Aleksey Nemov","age":24,"country":"Russia","year":2000,"date":"01/10/2000","sport":"Gymnastics","gold":2,"silver":1,"bronze":3,"total":6},
-          {"athlete":"Alicia Coutts","age":24,"country":"Australia","year":2012,"date":"12/08/2012","sport":"Swimming","gold":1,"silver":3,"bronze":1,"total":5},
-          {"athlete":"Missy Franklin","age":17,"country":"United States","year":2012,"date":"12/08/2012","sport":"Swimming","gold":4,"silver":0,"bronze":1,"total":5},
-          {"athlete":"Ryan Lochte","age":27,"country":"United States","year":2012,"date":"12/08/2012","sport":"Swimming","gold":2,"silver":2,"bronze":1,"total":5},
-          {"athlete":"Allison Schmitt","age":22,"country":"United States","year":2012,"date":"12/08/2012","sport":"Swimming","gold":3,"silver":1,"bronze":1,"total":5},
-          {"athlete":"Natalie Coughlin","age":21,"country":"United States","year":2004,"date":"29/08/2004","sport":"Swimming","gold":2,"silver":2,"bronze":1,"total":5},
-          {"athlete":"Ian Thorpe","age":17,"country":"Australia","year":2000,"date":"01/10/2000","sport":"Swimming","gold":3,"silver":2,"bronze":0,"total":5},
-          {"athlete":"Dara Torres","age":33,"country":"United States","year":2000,"date":"01/10/2000","sport":"Swimming","gold":2,"silver":0,"bronze":3,"total":5},
-          {"athlete":"Cindy Klassen","age":26,"country":"Canada","year":2006,"date":"26/02/2006","sport":"Speed Skating","gold":1,"silver":2,"bronze":2,"total":5},
-          {"athlete":"Nastia Liukin","age":18,"country":"United States","year":2008,"date":"24/08/2008","sport":"Gymnastics","gold":1,"silver":3,"bronze":1,"total":5},
-          {"athlete":"Marit Bjørgen","age":29,"country":"Norway","year":2010,"date":"28/02/2010","sport":"Cross Country Skiing","gold":3,"silver":1,"bronze":1,"total":5},
-          {"athlete":"Sun Yang","age":20,"country":"China","year":2012,"date":"12/08/2012","sport":"Swimming","gold":2,"silver":1,"bronze":1,"total":4},
-          {"athlete":"Kirsty Coventry","age":24,"country":"Zimbabwe","year":2008,"date":"24/08/2008","sport":"Swimming","gold":1,"silver":3,"bronze":0,"total":4}];
-
-        updateData(data);
+      onGridReady() {
+        // const httpRequest = new XMLHttpRequest();
+        this.rowData = this.$store.state.registration.agGridData;
 
         // axios.get('https://raw.githubusercontent.com/ag-grid/ag-grid/master/packages/ag-grid-docs/src/olympicWinnersSmall.json')
         //   .then((res) => {
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import authenticationModule from './modules/authentication';
+import registrationModule from './modules/registration';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-    authentication: authenticationModule
+    authentication: authenticationModule,
+    registration: registrationModule
   },
 })
